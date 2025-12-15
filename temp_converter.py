4a49acbe37381aed40f638885a1ff214d8e52cc1
P Likitha <likithapakala66@gmail.com>
2025-12-15 20:09:37 +0530

    Create temp_converter.py

---
+++ temp_converter.py
@@ -0,0 +1,17 @@
+value = float(input("Enter temperature value: "))
+unit = input("Enter unit (C/F/K): ").upper()
+
+if unit == "C":
+    print("Fahrenheit:", (value * 9/5) + 32)
+    print("Kelvin:", value + 273.15)
+
+elif unit == "F":
+    print("Celsius:", (value - 32) * 5/9)
+    print("Kelvin:", (value - 32) * 5/9 + 273.15)
+
+elif unit == "K":
+    print("Celsius:", value - 273.15)
+    print("Fahrenheit:", (value - 273.15) * 9/5 + 32)
+
+else:
+    print("Invalid unit! Use C, F, or K.")
