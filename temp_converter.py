8697fc9c124479f45602c57a1e25aca80ea08713
Likitha <likithapakala66@gmail.com>
2025-12-17 22:33:45 +0530

    updated temp_converter program

--- temp_converter.py
+++ temp_converter.py
@@ -7,11 +7,11 @@ if unit == "C":
 
 elif unit == "F":
     print("Celsius:", (value - 32) * 5/9)
-    print("Kelvin:", (value - 32) * 5/9 + 273.15)
+    #print("Kelvin:", (value - 32) * 5/9 + 273.15)
 
 elif unit == "K":
     print("Celsius:", value - 273.15)
-    print("Fahrenheit:", (value - 273.15) * 9/5 + 32)
+    #print("Fahrenheit:", (value - 273.15) * 9/5 + 32)
 
 else:
     print("Invalid unit! Use C, F, or K.")
